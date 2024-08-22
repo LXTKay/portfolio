796487eb19ad793d6c03d5344a14a5c61aa8b718
lxtk <thealextk@gmail.com>
2024-08-22 22:33:57 +0200

    changed some text

--- src/IntroBox.jsx
+++ src/IntroBox.jsx
@@ -7,7 +7,8 @@ export default function IntrooBox() {
     <div className="img">
       <div className="introBox">
         <h2>Herzlich Willkommen auf meinem Portfolio</h2>
-        <p>Hier finden Sie alle meine Projekte, die ich in irgendeiner Weise als zeigenswert erachte. Bei jedem Eintrag
+        <p>Hier finden Sie alle meine Projekte, die ich in irgendeiner Weise als zeigenswert erachte. Das Ganze ist
+          chronologisch geordnet. Bei jedem Eintrag
           finden Sie einen Link zur Live-Seite und zum Github Repo. Falls Sie zufällig hier gelandet sind und noch einen
           Fullstack Entwickler brauchen, schreiben Sie mir eine Nachricht, ich suche gerade Arbeit!
         </p>
--- src/cardData/portfolio.js
+++ src/cardData/portfolio.js
@@ -4,7 +4,7 @@ const card = {
   src: imgURL,
   headline: "Diese Seite",
   text: `Meine Portfolio-Website, auf der Sie sich gerade befinden. Ich habe React benutzt, weil die einzelnen \
-  Einträge das gleiche wiederverwertbare Component sind. Da ich hier nur ein paar andere Seiten verlinke, gibt es \
+  Einträge der gleiche wiederverwertbare Component sind. Da ich hier nur ein paar andere Seiten verlinke, gibt es \
   kein Backend. Das Hauptaugenmerk lag auf dem Design. Die Seite ist übrigens mobile-optimiert.`,
   githubLink: "https://github.com/LXTKay/portfolio",
 };
