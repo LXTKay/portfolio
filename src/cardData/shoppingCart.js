eb5a0b2503735b803782f5f20d54d4e9741830ea
lxtk <thealextk@gmail.com>
2024-08-23 12:59:28 +0200

    changed some styling and text

--- src/cardData/shoppingCart.js
+++ src/cardData/shoppingCart.js
@@ -4,7 +4,7 @@ const card = {
   src: imgURL,
   headline: "Online Shop",
   text: `Dies ist ein Frontend Projekt, dass eine Shoppingwebsite darstellt. Verschiedene Produkte werden via API gefetcht \
-  und unter dem Tab "Shop" angezeigt. Sie können Produkte auswählen, zum Warenkorb hinzufügen, aber nichts kaufen, da kein
+  und unter dem Tab "Shop" angezeigt. Sie können Produkte auswählen, zum Warenkorb hinzufügen, aber nichts kaufen, da kein \
   Backend existiert. Diese Website ist ein React Projekt.`,
   githubLink: "https://github.com/LXTKay/shoppingCarte",
   liveLink: "https://chipper-daffodil-d10897.netlify.app/shop"
